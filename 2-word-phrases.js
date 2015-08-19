9bdc753f8a9f96dbd8aae2db6c29647421cb229c
Johnnie <johnnielo50@gmail.com>
2015-08-19 15:56:20 -0700

    Created form to enter website. Also, function to make a get request.

--- 2-word-phrases.js
+++ 2-word-phrases.js
@@ -1,22 +1,46 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  Template.body.events({
+    "submit .process-website": function(event) {
+      // Prevents default broswer form submit
+      event.preventDefault(); 
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      // $(".disableOnSubmit").prop("disabled", true); 
+
+      // Gets value from from element 
+      var website = event.target.website.value; 
+
+      // Process URL 
+      Meteor.call("processWebsite", website, function(err, result) {
+        if (err) console.log(err); 
+
+        console.log("printing result in the callback " + result); 
+        // $(".disableOnSubmit").prop("disabled", false);
+      });
+
+      // Clear form
+      event.target.website.value = ""; 
     }
-  });
+  }); 
+
 }
 
+
+
 if (Meteor.isServer) {
+  Meteor.methods({
+    processWebsite: function(website) {
+      this.unblock(); 
+      Meteor.http.call("GET", website, function(error, result) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(result.content);
+        }
+      }); 
+      return website;
+    }
+  })
   Meteor.startup(function () {
     // code to run on server at startup
   });
